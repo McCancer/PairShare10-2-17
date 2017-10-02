fb82af9ef762ac4261ca31c54a43e4d519a7b2c8
Matthew C Kunzer <matthewkunzer@gmail.com>
2017-10-02 12:00:49 -0500

    sdg
    asdfasjk

---
+++ LinkedList.java
@@ -0,0 +1,87 @@
+import java.util.NoSuchElementException;
+/**
+   A linked list is a sequence of nodes with efficient
+   element insertion and removal. This class
+   contains a subset of the methods of the standard
+   java.util.LinkedList class.
+   Complete the reverse method by reversing the links in a
+   the list. Implement this method by directly rerouting the
+   links, not by using an iterator.
+*/
+public class LinkedList
+{
+   private Node first;
+
+   /**
+      Constructs an empty linked list.
+   */
+
+   /**
+      Reverses all elements in a linked list.
+   */
+   public void reverse()
+   {
+      // Complete this method
+      . . .
+
+
+   }
+
+   /**
+      Returns the first element in the linked list.
+      @return the first element in the linked list
+   */
+
+
+   /**
+      Removes the first element in the linked list.
+      @return the removed element
+   */
+
+
+   /**
+      Adds an element to the front of the linked list.
+      @param element the element to add
+   */
+
+   /**
+      Returns an iterator for iterating through this list.
+      @return an iterator for iterating through this list
+   */
+
+      /**
+         Constructs an iterator that points to the front
+         of the linked list.
+      */
+
+      /**
+         Moves the iterator past the next element.
+         @return the traversed element
+      */
+
+      /**
+         Tests if there is an element after the iterator
+         position.
+         @return true if there is an element after the iterator
+         position
+      */
+
+      /**
+         Adds an element before the iterator position
+         and moves the iterator past the inserted element.
+         @param element the element to add
+      */
+
+      /**
+         Removes the last traversed element. This method may
+         only be called after a call to the next() method.
+      */
+
+      /**
+         Sets the last traversed element to a different
+         value.
+         @param element the element to set
+      */
+
+
+}
